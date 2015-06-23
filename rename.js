7dd40dbed6fd49891c760ce48eb034af3b25ae66
Kevin Reed <kntreed@gmail.com>
2015-06-23 00:10:46 -0400

    WIP

--- rename.js
+++ rename.js
@@ -1,27 +1,25 @@
 var q = require('q'),
 	readline = require('readline'),
 	fs = require('fs'),
-	_ = require('lodash');
+	lo = require('lodash');
 	
 module.exports = function() {
-	
-	function getfiles(path, selector) {
-		var deferred = q.defer(),
-			results;
-		
-		fs.readdir(path, function(err, files) {
-			results = _.filter(files, selector);
-			deferred.resolve(results);
-		});
-		
-		return deferred.promise;
-	}
-	
-	// fs.rename(oldPath, newPath, function(err) {
-		
-	// });
-	
 	return {
-		getfiles: getfiles
+		getfiles: function(path, selector) {
+			var deferred = q.defer(),
+				results;
+
+			fs.readdir(path, function(err, files) {
+				console.log(files);
+				
+				results = lo.filter(files, selector);
+				deferred.resolve(results);
+			});
+			
+			return deferred.promise;
+		}
 	};
 };
+
+// fs.rename(oldPath, newPath, function(err) {		
+// });
