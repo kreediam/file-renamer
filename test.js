6fc1ee5e445bfc3201c18d306f70a69726b4f674
Kevin Reed <kntreed@gmail.com>
2015-07-01 06:16:43 -0400

    WIP

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 var rename = require('./rename')();
 
 rename.getfiles('C:\\temp', '.js')
-	.then(function(matches) {
-		console.log('Found ' + matches.length);
-	});
+.then(function(matches) {
+	console.log(matches);
+	//fs.rename(oldPath, newPath, callback)
+});
